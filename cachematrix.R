42befe1fa9a18e85cf3d3caacc67bbbed4a4775c
chris55555 <pysylp@gmail.com>
2015-03-22 07:46:59 -0400

    my project 2 work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of two functions get() and getinverse(); 
+# the function get() return the original matrix x;
+# and another function getinverse() which calculate its inversematrix and 
+# the result is stored in variable m
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x = numeric()) {
+  m <- NULL # set m default value
+  z<<-x
+  get <- function() x      # get original matrix x
+  
+  getinverse <- function(){
+    m<<-solve(x)
+    m    #returm inversematrix m
+  }
+  
+  list(get = get,getinverse = getinverse)  # return a list of two functions
 }
 
+   
+  ## cacheSolve: This function take the arguments y returned by makeCacheMatrix
+  # above. If the inversematrix has already been calculated (when m is not null), 
+  # then the cachesolve should retrieve the inverse from the cache m. Or it will
+  # calculate to give inverse matrix and return m.  
+  
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  cacheSolve <- function(y, ...) {
+    m <- y$getinverse()
+    if(!is.null(m)) {         # if m is not null, retrive inversematrix from m 
+      message("getting cached data")
+      return(m)  
+    }
+    data <- y$get()   # or, retrive original matrix and calculate its inversematrix and return m  
+    m <- solve(data, ...)
+    y$getinverse(m)
+    m
+  }
+  
